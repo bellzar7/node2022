c949be493899ed3deee4dbc761eac7790ccf466a
bellzar7 <belelya.nazar1999@gmail.com>
2022-08-11 15:59:20 +0300

    master

--- app.js
+++
@@ -1,6 +0,0 @@
-const module1 = require('./module1');
-
-require('./files')
-
-const user = module1.createUser('Nate', 21);
-user.sayHello()
--- files.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs').promises;
-
-fs.appendFile('./file.txt', 'Hello ept \n').then(() => {
-    console.log('done');
-}).catch(e => {
-    console.log(e);
-})
