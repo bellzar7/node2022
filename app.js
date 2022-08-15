c1d9300d950d47f854332ba61f07012eb083cbe8
bellzar7 <belelya.nazar1999@gmail.com>
2022-08-15 16:53:34 +0300

    hw1 done

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
+// const fs = require('fs/promises');
+// const path = require('path');
+//
+// const sortByFolder = async (dir, gen, newPath) => {
+//     try {
+//         const folderPath = path.join(__dirname, dir);
+//         const files = await fs.readdir(folderPath);
+//
+//         for (const file of files) {
+//
+//             const pathToFile = path.join(folderPath, file);
+//
+//             const buffer = await fs.readFile(pathToFile);
+//
+//             const user = JSON.parse(buffer.toString());
+//
+//             if (user.gender !== gen) {
+//               await  fs.rename(pathToFile, path.join(__dirname, newPath, file))
+//             }
+//         }
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+//
+// sortByFolder('boys', 'male', 'girls')
+// sortByFolder('girls', 'female', 'boys')
+
+// ADDITIONAL
 const fs = require('fs/promises');
 const path = require('path');
 
-const sortByFolder = async (dir, gen, newPath) => {
-    try {
-        const folderPath = path.join(__dirname, dir);
-        const files = await fs.readdir(folderPath);
+const reader = async (folderPath) => {
+    const files = await fs.readdir(folderPath);
 
-        for (const file of files) {
+    for (const file of files) {
+        const pathToFile = path.join(folderPath, file)
+        const stat = await fs.stat(pathToFile);
 
-            const pathToFile = path.join(folderPath, file);
-
-            const buffer = await fs.readFile(pathToFile);
-
-            const user = JSON.parse(buffer.toString());
-
-            if (user.gender !== gen) {
-              await  fs.rename(pathToFile, path.join(__dirname, newPath, file))
-            }
+        if (stat.isDirectory()) {
+            await reader(pathToFile)
         }
-    } catch (e) {
-        console.log(e);
+        if (stat.isFile()) {
+            await fs.rename(pathToFile, path.join(__dirname, 'Folder', file))
+        }
     }
+
 }
 
-sortByFolder('boys', 'male', 'girls')
-sortByFolder('girls', 'female', 'boys')
+reader(path.join(__dirname, 'Folder'))
